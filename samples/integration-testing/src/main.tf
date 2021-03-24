93d1466c4dfff2d588b4487bb8d51f07631506e6
Alexander Hughes <git@ahughes.me>
2021-03-24 16:00:16 -0600

    Commit to test ADO CI pipeline

--- samples/integration-testing/src/main.tf
+++ samples/integration-testing/src/main.tf
@@ -3,4 +3,7 @@ resource "random_uuid" "uuid" {}
 resource "azurerm_resource_group" "rg" {
   name     = "rg-hello-tf-${random_uuid.uuid.result}"
   location = var.location
+  tags = {
+    Deployment = "Azure DevOps"
+  }
 }
